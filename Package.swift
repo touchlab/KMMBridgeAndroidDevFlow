c59a5463369fa01abfa713508cbc265462b4669d
kpgalligan <kpgalligan@users.noreply.github.com>
2024-12-09 03:59:55 +0000

    KMP SPM package update

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.8
+import PackageDescription
+
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://api.github.com/repos/touchlab/KMMBridgeAndroidDevFlow/releases/assets/212031319.zip"
+let remoteKotlinChecksum = "1e6446b65302c2a31277873031854ba12f616b508870674e51e9e62e29fa5fce"
+let packageName = "kmplib"
+// END KMMBRIDGE BLOCK
+
+let package = Package(
+    name: packageName,
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: packageName,
+            targets: [packageName]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: packageName,
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
+        )
+        ,
+    ]
+)
