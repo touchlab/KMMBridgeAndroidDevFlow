2eed442b3ee4b3ec8565f168b5794e79a56801dd
kpgalligan <kpgalligan@users.noreply.github.com>
2024-12-09 15:34:25 +0000

    KMP SPM package update

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://api.github.com/repos/touchlab/KMMBridgeAndroidDevFlow/releases/assets/212031319.zip"
-let remoteKotlinChecksum = "1e6446b65302c2a31277873031854ba12f616b508870674e51e9e62e29fa5fce"
+let remoteKotlinUrl = "https://api.github.com/repos/touchlab/KMMBridgeAndroidDevFlow/releases/assets/212141026.zip"
+let remoteKotlinChecksum = "a4cd7b653d3aed8374d3d3cde7948bceb8d39c540bade7ba6319a4ba41fde2ac"
 let packageName = "kmplib"
 // END KMMBRIDGE BLOCK
 
