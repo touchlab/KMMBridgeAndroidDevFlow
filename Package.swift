e36d4f1e46ac0f9012a2ea8cf3cdedf53586414e
Kevin Galligan <kgalligan@gmail.com>
2024-12-10 16:28:05 -0500

    Slight changes

--- Package.swift
+++ Package.swift
@@ -1,11 +1,7 @@
 // swift-tools-version:5.8
 import PackageDescription
 
-// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://api.github.com/repos/touchlab/KMMBridgeAndroidDevFlow/releases/assets/212457259.zip"
-let remoteKotlinChecksum = "846a5a4c45fddc2ae0f6fc826478a0a9e0411cfca8607ab2efa97f69e67ea873"
 let packageName = "kmplib"
-// END KMMBRIDGE BLOCK
 
 let package = Package(
     name: packageName,
@@ -21,8 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            url: remoteKotlinUrl,
-            checksum: remoteKotlinChecksum
+            path: "./kmplib/build/XCFrameworks/debug/\(packageName).xcframework"
         )
         ,
     ]
